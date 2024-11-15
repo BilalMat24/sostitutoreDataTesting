d56f1db52af51adfba4a78812d911aa990c14c83
BilalMat24 <bilal.biaz@mat.tn.it>
2024-11-15 13:53:23 +0100

    codice che fa il loop molteplici volte, sia per ora che per minuto

--- src/SostitutoreDate.java
+++ src/SostitutoreDate.java
@@ -4,33 +4,63 @@ import java.util.Date;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.util.Scanner;
 
 public class SostitutoreDate {
-
+    private static final String cam100 = "_cam100";
     private static final String cam101 = "_cam101";
-    /*private static int SECONDI = 8640*/;
+    private static final String cam102 = "_cam102";
+    private static final String cam103 = "_cam103";
+    private static final String cam104 = "_cam104";
+    private static final String cam105 = "_cam105";
+    private static final String cam106 = "_cam106";
+    private static final String cam107 = "_cam107";
+    private static final String cam108 = "_cam108";
+    private static final String cam109 = "_cam109";
 
+    private static int GIRI = 6; // il programma elabora 10 img al secondo, quindi 6 giri sono 60 img al minuto
+    private static int WAIT = 1000; // aspetto 1000 millisecondi(1s)
     public static void main(String[] args) {
 
+        // File[] files = getCartella().listFiles() spostato all'interno del primo for così viene aggiornato ad ongi ciclo con i nuovi nomi delle img
 
+        Scanner scanner = new Scanner(System.in); //System.in is a standard input stream
+        System.out.print("Digita ora per le Ore oppure minuti per i Minuti: ");
+        String c = scanner.nextLine();
+        int minuto = 0;
+        if (c.equals("minuti")) {
 
+            System.out.print("Scegli minuti: ");
+            int scegliMinuto = scanner.nextInt();
+            minuto = GIRI * scegliMinuto;
+            System.out.println("Il valore in secondi è: " + (minuto * 10));
+            scanner.close();
+            System.out.print("Scegli minuti: ");
+        } else if (c.equals("ora")) {
+
+            System.out.print("Scegli Ora: ");
+            int scegliOra = scanner.nextInt();
+            int ora = GIRI * (scegliOra*60) ;
+            System.out.println("Il valore in secondi è: " + (ora * 10));
+            scanner.close();
+            minuto = ora;
+        }
         boolean rename101 = false;
-        int secondi = 8640;
-        for(int j = 0; j < secondi; j++) {
-            File[] files = getCartella().listFiles();
-            System.out.println("nella cartella ci sono " + files.length + " file");
+        for (int j = 0; j < minuto; j++) {
+            File[] files = getCartella().listFiles(); // spostato all interno del primo in modo tale che venga aggiornato
+            System.out.println("copiate: " + (j * 10) + " immagini");
             for (int i = 0; i < files.length; i++) {
                 if (files[i].isFile()) {
 
                     File newFile101 = sostituisciNomeImmagine101(files[i]);
 
-                    rename101 = files[i].renameTo(newFile101);
+                    rename101 = files[i].renameTo(newFile101);   // Da sistemare
 
                     System.out.println(files[i]);
 
                     try {
                         Path destPath101 = getCartella101().toPath().resolve(newFile101.getName());
-                        Files.copy(newFile101.toPath(), destPath101, StandardCopyOption.REPLACE_EXISTING);
+                        Files.copy(newFile101.toPath(), destPath101 /*StandardCopyOption.REPLACE_EXISTING*/);
                         System.out.println("File copiato in: " + destPath101);
 
                     } catch (IOException e) {
@@ -38,7 +68,7 @@ public class SostitutoreDate {
                         e.printStackTrace();
                     }
                     try {
-                        Thread.sleep(1000);
+                        Thread.sleep(WAIT);
                     } catch (Exception e) {
                         throw new RuntimeException(e);
                     }
@@ -64,6 +94,7 @@ public class SostitutoreDate {
     }
 
 
+
     public static File sostituisciNomeImmagine101(File file1) {
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
         String timestamp = dateFormat.format(new Date());
@@ -71,14 +102,22 @@ public class SostitutoreDate {
     }
 
     //Metodo non utlizzato
-/*    public static File[] sostituisciNomeImmagineDinamico(File[] file){
+    public static File[] sostituisciNomeImmagineDinamico(File[] file){
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
         String timestamp = dateFormat.format(new Date());
-        File cam101File = new File(getCartella() + "/2024725000_" + timestamp + cam101 + ".jpg");
         File cam100XFile = new File(getCartella() + "/2024725000_" + timestamp + cam100 + ".jpg");
-        File cam10XFile = new File(getCartella() + "/2024725000_" + timestamp + cam10 + ".jpg");
-        File[] cams = {cam10XFile,cam100XFile, cam101File};
+        File cam101XFile = new File(getCartella() + "/2024725000_" + timestamp + cam101 + ".jpg");
+        File cam102XFile = new File(getCartella() + "/2024725000_" + timestamp + cam102 + ".jpg");
+        File cam103XFile = new File(getCartella() + "/2024725000_" + timestamp + cam103 + ".jpg");
+        File cam104XFile = new File(getCartella() + "/2024725000_" + timestamp + cam104 + ".jpg");
+        File cam105XFile = new File(getCartella() + "/2024725000_" + timestamp + cam105 + ".jpg");
+        File cam106XFile = new File(getCartella() + "/2024725000_" + timestamp + cam106 + ".jpg");
+        File cam107XFile = new File(getCartella() + "/2024725000_" + timestamp + cam107 + ".jpg");
+        File cam108XFile = new File(getCartella() + "/2024725000_" + timestamp + cam108 + ".jpg");
+        File cam109XFile = new File(getCartella() + "/2024725000_" + timestamp + cam109 + ".jpg");
+
+        File[] cams = {cam100XFile,cam101XFile, cam102XFile, cam103XFile, cam104XFile ,cam105XFile, cam106XFile, cam107XFile, cam108XFile, cam109XFile};
         return cams;
-    }*/
+    }
 
 }
